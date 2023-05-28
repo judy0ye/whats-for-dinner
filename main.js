4961fe545f53b44a65b943967270c38cf6e281ce
Judy Ye <judyy.ye@gmail.com>
2023-05-27 21:39:23 -0700

    fix: Block same favorite recipes from appearing on favorites box after clicking back to main and into favorites page again

--- main.js
+++ main.js
@@ -61,23 +61,34 @@ var required = document.querySelector('.required')
 var favoritesPage = document.querySelector('.favorites-page')
 var mainPage = document.querySelector('.flex-container')
 var favoritesBox = document.querySelector('.favorites-box')
+var list = document.getElementById('list')
 
 // buttons
 var cookButton = document.querySelector('.cook')
-var addToFavoritesButton = document.getElementById('favorite')
 var addToFavoritesButton = document.querySelector('.add-to-favorites')
 var hiddenFavoritesButtons = document.querySelector('.favoritesButtons-flex-container')
 var viewFavoritesButton = document.querySelector('.view-favorites')
+var backToMainButton = document.querySelector('.back-to-main')
 
 // event listeners
 cookButton.addEventListener('click', function() {
     showRandomDish()
     hiddenFavoritesButtons.classList.remove('hidden')
 })
-
 addToFavoritesButton.addEventListener('click', addToFavoriteRecipes)
 viewFavoritesButton.addEventListener('click', showFavoritesPage)
+backToMainButton.addEventListener('click', backToMain)
 
+window.addEventListener('DOMContentLoaded', function() {
+    var deleteButton = document.querySelector('.delete')
+    if (deleteButton) {
+        deleteButton.addEventListener('click', function(e) {
+        deleteRecipe(e)  
+    })
+}
+}) 
+    
+  
 // functions
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
@@ -113,10 +124,8 @@ function showRandomDish() {
         <h4 id="you-should-make"><i>You should make:</i></h4>
         <p style="font-size:25px">${currentRecipe}</p>
     </section>` 
-        
-    //anotherOneButton.classList.remove('hidden')
 }
-{/* <section id="current-recipe"></section> */}
+
 
 function addToFavoriteRecipes() {
     if (!favoriteRecipes.includes(currentRecipe)) {
@@ -125,6 +134,7 @@ function addToFavoriteRecipes() {
 }
 
 function showFavoritesPage() {
+    favoritesBox.innerHTML = ''
     favoritesPage.classList.remove('hidden')
     mainPage.classList.add('hidden')
 
@@ -132,10 +142,31 @@ function showFavoritesPage() {
         favoritesBox.innerHTML +=
         `<div class="favorite-recipe-list">
         <ul>
-            <li>${favoriteRecipes[i]}</li>
+            <li id="list">
+                ${favoriteRecipes[i]}
+                <span>
+                    <button class="delete">Delete</button>
+                </span>
+            </li>
         </ul>
         </div>`
     }
-    
+}
 
+function backToMain() {
+    favoritesPage.classList.add('hidden')
+    mainPage.classList.remove('hidden')
+}
+
+function deleteRecipe(e) {
+    if (e.target.classList.contains('delete')) {
+        list.removeChild(e.target.parentElement.parentElement.parentElement)
+        list.removeChild(list)
+    }
+    // for (var i = 0; i < favoriteRecipes.length; i++) {
+    //     favoriteRecipes[i].closest('li').remove()
+    // }
+    // if (e.target.classList.contains('delete')) {
+    //     list.parentNode.parentNode.parentNode.removeChild(list)
+    // }
 }
